da55d7befae356fff44d349855c84620df8274a6
Giselle Ponce <gponce@amco.com.ar>
2021-01-06 16:56:15 -0300

    se crea proyecto

---
+++ src/java/user/java.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package user;
+
+/**
+ *
+ * @author GisellePonce
+ */
+public class java {
+    
+   private int id;
+   private String cuit;
+   private String numero_registro;
+   private String estado;
+   private Double monto;
+    
+    
+}
